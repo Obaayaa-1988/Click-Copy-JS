7d311263aa93a2cef93dd3023fb7b3e2af18f42e
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-04-22 22:52:00 +0100

    added es6 function for click copy logic

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const btn = document.querySelector('.btn');
 const coupon = document.querySelector('.coupon');
 
+
+
+const copyText = (e) =>{
+    e.preventDefault(e);
+
+    coupon.select()
+}
 btn.addEventListener("click", copyText)
