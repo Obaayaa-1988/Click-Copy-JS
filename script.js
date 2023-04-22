0b610fa104e3dc74c46442e8e0dde41331bc3094
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-04-22 22:56:57 +0100

    added set timeout and text content to function btn

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const coupon = document.querySelector('.coupon');
 const copyText = (e) =>{
     e.preventDefault(e);
 
-    coupon.select()
+    coupon.select();
+    coupon.setSelectionRange(0, 999999);
+    document.execCommand("copy");
+
+    btn.textContent = "Copied!!!"
+    setTimeout(() => {
+        btn.textContent = "Copy"
+    }, 3000)
 }
+
 btn.addEventListener("click", copyText)
